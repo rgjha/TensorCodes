d19b1d748c8988a512cb1e0978d59906398d1340
rgjha <rgjha1989@gmail.com>
2019-11-24 22:16:08 -0500

    Step forward. Still far.

--- 2dXY.py
+++ 2dXY.py
@@ -31,8 +31,8 @@ beta = float(1.0/Temp)
 # Improvement by never explicitly constructing M --> D=27 is 8 seconds, 
 # D=32 is 28 sec, D=36 is 58 sec, D=40 takes about 160 sec. 
 
-D=35
-D_cut=35
+D=25
+D_cut=25
 Niters=8
 Ns = int(2**((Niters)))
 Nt = Ns  
@@ -48,11 +48,11 @@ if D%2 == 0:
 Dn = int(D/2.0)
 
 ##### Set bond dimensions and options
-chiM = 22
-chiS = 22
-chiU = 22
-chiH = 22      
-chiV = 22
+chiM = 25
+chiS = 25
+chiU = 25
+chiH = 25      
+chiV = 25
 #Increasing these makes it more accurate!        
 
 
@@ -75,6 +75,14 @@ A = np.zeros([D])
 L = np.zeros([D])              
 ATNR = [0 for x in range(numlevels+1)];
 ATNRnorm = [0 for x in range(numlevels+1)]; 
+
+
+##############################
+def zeta_proxy(A):
+    w, U = LA.eigh(A) 
+    idx = w.argsort()[::-1]
+    s = w[idx]
+    return 1.0/(np.log(s[0]/s[1]))
 
 
 ##############################
@@ -196,6 +204,32 @@ if __name__ == "__main__":
             doTNR(ATNR[k],[chiM,chiS,chiU,chiH,chiV], 1e-8, 1000, 100, True, 0.1)
             #print('RGstep: %d, Truncation Errors: %e, %e, %e, %e' % \
             #    (k+1,SPerrs[k,0],SPerrs[k,1],SPerrs[k,2],SPerrs[k,3]))
+            Tmp = ATNR[k+1] 
+            norm = ATNRnorm[k+1]
+
+
+        #X = (np.einsum('iikk',Tmp))**2
+        #X1 = X/np.einsum('ijkk, jill',Tmp, Tmp)
+        #X1 = X/ncon([Tmp,Tmp],[[1,2,3,3],[2,1,4,4]])
+        #Y1 = X/np.einsum('ijkl, jilk',Tmp, Tmp)     # X2 in 0903.1069 and X in 1706.03455
+        #Y1 = X/ncon([Tmp,Tmp],[[1,2,3,4],[2,1,4,3]])
+
+
+        #ts_TN_A0 = norm * Tmp   # Restore back last normalization!
+
+        #tmp1 = np.einsum('ijkl,jiqr -> kqlr ',Tmp, Timpure)
+        #tmp1 = tmp1.reshape(tmp1.shape[0]**2, tmp1.shape[0]**2)
+        #tmp2 = np.einsum("ii", tmp1)
+
+        #M1 = ncon((ts_TN_A0),([-2,1,-1,1]))  # See 0903.1069
+        #zeta1 = zeta_proxy(M1)
+        #print ("ξ1 is ", zeta1)
+
+        #M2 = ncon((ts_TN_A0),([-1,-3,-2,-4]))  # Order matters? --> yes!
+        #dum = M2.shape[0] 
+        #M2 = M2.reshape(dum**2, dum**2)
+        #zeta2 = zeta_proxy(M2)
+        #print ("ξ2 is ", zeta2)
 
 
             ''' 
@@ -216,6 +250,34 @@ if __name__ == "__main__":
             Vgauge = ncon([wC[k-1],wC[k-1]],[[1,2,-1],[2,1,-2]])
             FreeEnergy[k-1] = -1.0*(sum((4**np.int64(np.array(range(k,-1,-1))))*np.log(ATNRnorm[:(k+1)]))+ \
             np.log(ncon([ATNR[k],Hgauge,Vgauge],[[1,3,2,4],[1,2],[3,4]])))
+
+
+        for k in range(1,numlevels+1): 
+            Hgauge = ncon([vC[k-1],vC[k-1]],[[1,2,-1],[2,1,-2]])
+            Vgauge = ncon([wC[k-1],wC[k-1]],[[1,2,-1],[2,1,-2]])
+            FreeEnergy[k-1] = -1.0*(sum((4**np.int64(np.array(range(k,-1,-1))))*np.log(ATNRnorm[:(k+1)]))+ \
+            np.log(ncon([ATNR[k],Hgauge,Vgauge],[[1,3,2,4],[1,2],[3,4]])))
+
+        ##### Change gauge on disentanglers 'u'
+        a = int(np.sqrt(chiM))
+        gaugeX = np.eye(chiM).reshape(a,a,a,a).transpose(1,0,2,3).reshape(chiM,chiM)
+        upC = [0 for x in range(numlevels)]
+        upC[0] = ncon([uC[0],gaugeX],[[-1,1,-3,-4],[1,-2]])
+        for k in range(1,numlevels):
+            uF,sF,vhF = LA.svd(ncon([wC[k-1],wC[k-1]],[[1,2,-1],[2,1,-2]]))
+            gaugeX = uF @ vhF
+            upC[k] = ncon([uC[k],gaugeX],[[-1,1,-3,-4],[1,-2]])
+
+
+
+        for k in range(numlevels):
+            sXcg[k+1] = ncon([sXcg[k],upC[k],upC[k],wC[k],wC[k],wC[k],wC[k]],\
+            [[3,4,1,2],[1,2,6,9],[3,4,7,10],[5,6,-1],[9,8,-2],[5,7,-3],[10,8,-4]])
+    
+        dtemp = LA.eigvalsh(sXcg[numlevels].reshape((sXcg[numlevels].shape[0])**2,(sXcg[numlevels].shape[2])**2))
+        ExpectX = max(abs(dtemp))
+
+
 
         print ("Temperature is", Temp, "and free energy using TNR is", FreeEnergy[numlevels-1]/(beta*(4**numlevels)))
 
