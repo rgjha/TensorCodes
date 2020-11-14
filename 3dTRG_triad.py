3116539ba08a00a7886caabd61b05db9d34de42c
rgjha <rgjha1989@gmail.com>
2020-11-14 12:49:49 +0530

    Update triad-1

--- 3dTRG_triad.py
+++ 3dTRG_triad.py
@@ -19,6 +19,8 @@ import time
 import datetime 
 from ncon import ncon
 
+
+
 startTime = time.time()
 print ("STARTED: " , datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) 
 
@@ -28,8 +30,8 @@ if len(sys.argv) < 2:
 
 Temp =  float(sys.argv[1])
 beta = float(1.0/Temp)
-Niter=4
-Dcut=5
+Niter=2
+Dcut=25   # Result unchanged D=32 onwards! Reasons unknown. 
 
 
 def dagger(a):
@@ -83,7 +85,76 @@ def Z3d_Ising():
 
 
 
-#def coarse_graining(input,impure=False):
+def coarse_graining(in1, in2, in3, in4,impure=False):
+
+
+    A = in1
+    B = in2
+    C = in3
+    D = in4 
+
+    T = np.einsum("ika, amb, bnc, clj -> ijklmn", A, B, C, D)
+
+    T = T.transpose(0,3,1,2,4,5)
+    print ("Start shape", np.shape(T))
+    # Eq (3) of arXiv:1912.02414
+
+    M = ncon([T,T],[[-1,-3,-5,-7,1,-9],[-2,-4,-6,-8,-10,1]])
+    a = np.shape(M)[0]*np.shape(M)[1] 
+    b = np.shape(M)[2]*np.shape(M)[3] 
+    e = np.shape(M)[4]*np.shape(M)[5] 
+    f = np.shape(M)[6]*np.shape(M)[7] 
+    c = np.shape(M)[8]
+    d = np.shape(M)[9]
+    M1 = np.reshape(M,(a,b,e,f,c,d))
+    Mprime = np.reshape(M1,(a,(b*e*f*c*d)))
+    K = np.dot(Mprime, dagger(Mprime))
+
+    az = int(np.sqrt(np.shape(K)[0])) 
+
+    #K1 = np.reshape(K,(az*az,az*az))
+    K2 = np.reshape(K,(az,az,az,az))
+
+
+    #U, s1, UL = LA.svd(K1)
+    #print ("SH", np.shape(U), np.shape(s1), np.shape(UL))
+    U, s1, UL = tensorsvd(K2,[0,1],[2,3],Dcut)  # This is U [Eq. '11' of arXiv:1912.02414] 
+    #print ("SH2", np.shape(U), np.shape(s1), np.shape(UL))
+
+
+    Mprime = np.reshape(M1,(e,a*b*f*c*d))
+    K = np.dot(Mprime, dagger(Mprime))
+
+    az = int(np.sqrt(np.shape(K)[0])) 
+    K = np.reshape(K,(az,az,az,az))
+
+    V, s2, VL = tensorsvd(K,[0,1],[2,3],Dcut)  # This is V of arXiv:1912.02414 
+
+
+    print ("Shapess", np.shape(C), np.shape(D), np.shape(U), np.shape(V))
+    UC = np.einsum("azc, cqp, pix, bji, qjy -> abyzx", C, D, U, D, V)   
+    print ("OK")
+    MC = np.einsum("awc, bwd -> abcd", B, C)
+    DC = np.einsum("dzb, pix, pqa, qjy, ijd -> zxyab", B, np.conjugate(U), A, np.conjugate(V), A)
+    #T1 = np.einsum("zxyae, aebf, bfijk -> zjxkyi", DC, MC, UC)
+    #print ("End shape", np.shape(T1), LA.norm(T1))
+
+
+
+    Tmp = np.einsum("abcd, cdyxz -> abyxz", MC, UC)
+    Gprime, st, D = tensorsvd(Tmp,[0,1,2],[3,4],Dcut)
+
+    #Gprime = np.einsum("abcd, de -> abce", Gprime, st) # Added this later! May be wrong!
+    #Eq20 = np.einsum("abyg, gxz -> abyxz", Gprime, Dprime)
+
+
+    Tmp2 = np.einsum("zxyab, abig -> zxyig", DC, Gprime)
+    A, st2, MCprime = tensorsvd(Tmp2,[0,1],[2,3,4],Dcut) 
+    #Eq21 = np.einsum("zxe, eyig -> zxyig", Aprime, MCprime)
+    B, st3, C = tensorsvd(MCprime,[0,1],[2,3],Dcut)
+
+
+    return A,B,C,D 
 
 
 if __name__ == "__main__":
@@ -94,83 +165,21 @@ if __name__ == "__main__":
     W = np.array([[a,b],[a,-b]])
     Id = np.eye(2) 
 
-
     A = np.einsum("ax, ay -> xya", W, W)
     B = np.einsum("ab, az -> azb", Id, W)
     C = np.einsum("bc, bz -> bzc", Id, W)
     D = np.einsum("cy, cx -> cyx", W, W)
 
-    T = np.einsum("ika, amb, bnc, clj -> ijklmn", A, B, C, D)
-    print ("Start shape", np.shape(T))
-    # Eq (3) of arXiv:1912.02414
+
+    for iter in range (Niter):
+
+        A, B, C, D = coarse_graining(A,B,C,D) 
+        A, B, C, D = coarse_graining(A,B,C,D) 
 
 
-    M = ncon([T,T],[[-1,-3,-5,-7,1,-9],[-2,-4,-6,-8,-10,1]])
-    a = np.shape(M)[0] 
-    b = np.shape(M)[2]
-    e = np.shape(M)[4]
-    f = np.shape(M)[6]
-    c = np.shape(M)[8]
-    d = np.shape(M)[9]
-    M1 = np.reshape(M,(a**2,b**2,e**2, f**2, c,d))
-    M = M1 
-    Mprime = np.reshape(M,(a**2,(b**2)*(e**2)*(f**2)*c*d))
-    K = np.matmul(Mprime, dagger(Mprime))
+    # ........................................................
 
-    K = np.reshape(K,(2,2,2,2))
-    U1, s1, V1 = tensorsvd(K,[0,1],[2,3],Dcut)  # This is U of arXiv:1912.02414
-
-
-    Mprime = np.reshape(M,(b**2,(a**2)*(e**2)*(f**2)*c*d))
-    K = np.matmul(Mprime, dagger(Mprime))
-    K = np.reshape(K, (2,2,2,2))
-    U2, s2, V2 = tensorsvd(K,[0,1],[2,3],Dcut)  # This is V of arXiv:1912.02414 
-
-    UC = np.einsum("azc, cqp, pix, bji, qjy -> abyzx", C, D, U1, D, U2)   
-    MC = np.einsum("awc, bwd -> abcd", B, C)
-    DC = np.einsum("dzb, pix, pqa, qjy, ijd -> zxyab", B, np.conjugate(U1), A, np.conjugate(U2), A)
-    T1 = np.einsum("zxyae, aebf, bfijk -> zjxkyi", DC, MC, UC)
-    print ("End shape", np.shape(T1), LA.norm(T1))
-
-    Tmp = np.einsum("abcd, cdyxz -> abyxz", MC, UC)
-    Gprime, st, Dprime = tensorsvd(Tmp,[0,1,2],[3,4],Dcut)
-    Eq20 = np.einsum("abyg, gxz -> abyxz", Gprime, Dprime)
-
-    Tmp2 = np.einsum("zxyab, abig -> zxyig", DC, Gprime)
-    Aprime, st2, MCprime = tensorsvd(Tmp2,[0,1],[2,3,4],Dcut)
-    Eq21 = np.einsum("zxe, eyig -> zxyig", Aprime, MCprime)
-
-    Bprime, st3, Cprime = tensorsvd(MCprime,[0,1],[2,3],Dcut)
-
-    T2 = np.einsum("zxa, ayb, bic, cjk -> zkxjyi", Aprime, Bprime, Cprime, Dprime)
-    T3 = T2.transpose(0,3,1,2,4,5)
-    print ("End shape", np.shape(T3), LA.norm(T3))
-    # Without truncation norm should match for T1 and T3. 
-    # Not yet!
-
-
-
-
-
-
-
-    '''
-    for i in range(Niter):
-
-        T, TI, norm = coarse_graining(T, False)
-        C = np.log(norm)+6*C 
-        N *= 6  # 3d has six open legs 
-
-
-        if i == Niter-1:
-
-            Z = final_step(T, False)
-            f = -Temp*(np.log(Z)+6*C)/(6*N)
-            #f = -(np.log(Z)+6*C)/(6*N)
-    '''
     print ("COMPLETED: " , datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 
             
-
-        
