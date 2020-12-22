ef95dc29bfae006035aee952d76ad0ced3595e79
Raghav G. Jha <rgjha@users.noreply.github.com>
2020-12-22 16:10:36 -0500

    Julia code is slow! Need to strip einsum with something fast.

--- 3d/3dTRG_Ising.jl
+++ 3d/3dTRG_Ising.jl
@@ -2,6 +2,8 @@
 # Check for tensor computations:
 # https://jutho.github.io/TensorOperations.jl/stable/indexnotation/#Index-notation-with-macros
 # A useful MATLAB-Python-Julia cheatsheet is https://cheatsheets.quantecon.org/
+# Niter=15 and Dcut=30 takes 1619 seconds while it takes 480 seconds in Python.
+# Seems like should get rid of "einsums" completely.
 
 import Pkg; Pkg.add("Einsum")
 using LinearAlgebra, Statistics, TensorOperations, Einsum, Dates
@@ -62,9 +64,7 @@ end
 
 function Z3d_Ising(beta)
 
-    a = sqrt(cosh(beta))
-    b = sqrt(sinh(beta))
-    W = [a b; a -b]
+    W = [sqrt(cosh(beta)) sqrt(sinh(beta)); sqrt(cosh(beta)) -sqrt(sinh(beta))]
     #out = np.einsum("ia, ib, ic, id, ie, if -> abcdef", W, W, W, W, W, W)
     #@tensor out[:] := W[1,-1]*W[1,-2]*W[1,-3]*W[1,-4]*W[1,-5]*W[1,-6]
     #Id = Matrix{Float64}(I, 2, 2)
@@ -80,14 +80,10 @@ end
 
 function coarse_graining(in1, in2, in3, in4)
 
-    A = in1
-    B = in2
-    C = in3
-    D = in4
 
     #S2 = contract('dze,izj->diej', B, conjugate(B))
     #@tensoropt S2[d,i,e,j] = B[d,z,e]* conj(B)[i,z,j]
-    S2 = @ncon([B, conj(B)],[[-1,1,-3],[-2,1,-4]])
+    S2 = @ncon([in2, conj(in2)],[[-1,1,-3],[-2,1,-4]])
 
     a = size(S2,1) * size(S2,2)
     b = size(S2,3) * size(S2,4)
@@ -95,20 +91,20 @@ function coarse_graining(in1, in2, in3, in4)
 
     #Tmp = contract('fyx,iyx->fi', D, np.conjugate(D))
     #@tensoropt Tmp[f,i] = D[f,y,x]* conj(D)[i,y,x]
-    Tmp = @ncon([D, conj(D)],[[-1,1,2],[-2,1,2]])
+    Tmp = @ncon([in4, conj(in4)],[[-1,1,2],[-2,1,2]])
     # Seems like we need to replace tensoropt with NCON
     # NCON doesn't need preallocated space. tensoropt does!
 
     #R2 = contract('ewf,ijk,fk->eiwj', C, np.conjugate(C), Tmp)
 
     #@tensoropt R2[e,i,w,j] = C[e,w,f]* conj(C)[i,j,k]* Tmp[f,k]
-    R2 = @ncon([C, conj(C), Tmp],[[-1,-3,1],[-2,-4,2],[1,2]])
+    R2 = @ncon([in3, conj(in3), Tmp],[[-1,-3,1],[-2,-4,2],[1,2]])
     a = size(R2,1) * size(R2,2)
     b = size(R2,3) * size(R2,4)
     R2mat = reshape(R2,(a,b))
 
     #S1 = contract('xyd,iyj->xidj', A, np.conjugate(A))
-    S1 = @ncon([A, conj(A)],[[-1,1,-3],[-2,1,-4]])
+    S1 = @ncon([in1, conj(in1)],[[-1,1,-3],[-2,1,-4]])
     #@tensoropt S1[x,i,d,j] = A[x,y,d]* conj(A)[i,y,j]
     a = size(S1,1) * size(S1,2)
     b = size(S1,3) * size(S1,4)
@@ -127,7 +123,7 @@ function coarse_graining(in1, in2, in3, in4)
 
     #R3 = contract('awb,ijk,bk->aiwj', B, np.conjugate(B), Tmp)
     #@tensoropt R3[a,i,w,j] = B[a,w,b]* conj(B)[i,j,k]* Tmp[b,k]
-    R3 = @ncon([B, conj(B), R2],[[-1,-3,1],[-2,-4,2],[1,2]])
+    R3 = @ncon([in2, conj(in2), R2],[[-1,-3,1],[-2,-4,2],[1,2]])
     a = size(R3,1) * size(R3,2)
     b = size(R3,3) * size(R3,4)
     R3mat = reshape(R3,(a,b))
@@ -143,7 +139,7 @@ function coarse_graining(in1, in2, in3, in4)
     # Now finding "V"
     #S1 = contract('ijk,ipq->jpkq', A, np.conjugate(A))
     #@tensoropt S1[j,p,k,q] = A[i,j,k]* conj(A)[i,p,q]
-    S1 = @ncon([A, conj(A)],[[1,-1,-3],[1,-2,-4]])
+    S1 = @ncon([in1, conj(in1)],[[1,-1,-3],[1,-2,-4]])
 
     a = size(S1,1) * size(S1,2)
     b = size(S1,3) * size(S1,4)
@@ -153,7 +149,7 @@ function coarse_graining(in1, in2, in3, in4)
 
     #@tensoropt R3[i,p,j,q] = B[i,j,k]* conj(B)[p,q,r]* Tmp[k,r]
 
-    R3 = @ncon([B, conj(B), R2],[[-1,-3,1],[-2,-4,2],[1,2]])
+    R3 = @ncon([in2, conj(in2), R2],[[-1,-3,1],[-2,-4,2],[1,2]])
 
     a = size(R3,1) * size(R3,2)
     b = size(R3,3) * size(R3,4)
@@ -162,7 +158,8 @@ function coarse_graining(in1, in2, in3, in4)
     #Kprime = contract('ia,ab,bc,cd,de',S1,S2,R2mat,R3mat.T,S1.T)
     #@tensoropt Kprime[i,e] = S1[i,a]*S2[a,b]*R2mat[b,c]*transpose(R3mat)[c,d]*transpose(S1)[d,e]
 
-    Kprime = @ncon([S1,S2,R2mat,transpose(R3mat),transpose(S1)],[[-1,1],[1,2],[2,3],[3,4],[4,-2]])
+    #Kprime = @ncon([S1,S2,R2mat,transpose(R3mat),transpose(S1)],[[-1,1],[1,2],[2,3],[3,4],[4,-2]])
+    Kprime = S1 * S2 * R2mat * transpose(R3mat) * transpose(S1)
 
     a = Int(sqrt(size(Kprime,1)))
     b = Int(sqrt(size(Kprime,2)))
@@ -173,8 +170,8 @@ function coarse_graining(in1, in2, in3, in4)
     #@tensoropt Tmp2[b,i,q,y] = D[b,j,i]* V[q,j,y]
     #@tensoropt Tmp3[c,x,b,y] = Tmp1[c,q,i,x]* Tmp2[b,i,q,y]
 
-    Tmp1 = @ncon([D, U],[[-1,-2,1],[1,-3,-4]])
-    Tmp2 = @ncon([D, V],[[-1,1,-2],[-3,1,-4]])
+    Tmp1 = @ncon([in4, U],[[-1,-2,1],[1,-3,-4]])
+    Tmp2 = @ncon([in4, V],[[-1,1,-2],[-3,1,-4]])
     Tmp3 = @ncon([Tmp1, Tmp2],[[-1,1,2,-2],[-3,2,1,-4]])
 
     #Tmp1 = contract('cqp,pix -> cqix',D,U)
@@ -184,13 +181,13 @@ function coarse_graining(in1, in2, in3, in4)
     #UC = contract('azc,cxby -> abyxz',C,Tmp3)
 
 
-    MC = @ncon([B, C],[[-1,1,-3],[-2,1,-4]])
+    MC = @ncon([in2, in3],[[-1,1,-3],[-2,1,-4]])
 
     #MC = contract('ijk,pjr->ipkr', B, C)
     #Tmp = contract('ijab,azc,cxby->ijyxz', MC, C, Tmp3)
 
-    Tmp = @ncon([MC, C, Tmp3],[[-1,-2,1,2],[1,-5,3],[3,-4,2,-3]])
-    G, st, D = tensorsvd(Tmp,[1,2,3],[4,5],5)
+    Tmp = @ncon([MC, in3, Tmp3],[[-1,-2,1,2],[1,-5,3],[3,-4,2,-3]])
+    G, st, out4 = tensorsvd(Tmp,[1,2,3],[4,5],5)
 
     #G = @ncon([G, st],[[-1,-2,-3,1],[1,-4]])
     @einsum   G[i,j,k,l] = G[i,j,k,a] * st[a,l]
@@ -199,53 +196,52 @@ function coarse_graining(in1, in2, in3, in4)
 
     # DC = B_dzb * U*_pix * A_pqa * V*_qjy * A_ijd
     #Tmp1 = contract('pix,pqa->ixqa', np.conjugate(U), A)
-    Tmp1 = @ncon([conj(U), A],[[1,-1,-2],[1,-3,-4]])
+    Tmp1 = @ncon([conj(U), in1],[[1,-1,-2],[1,-3,-4]])
 
 
-    Tmp2 = @ncon([conj(V), A],[[-1,1,-2],[-3,1,-4]])
+    Tmp2 = @ncon([conj(V), in1],[[-1,1,-2],[-3,1,-4]])
     #Tmp2 = contract('qjy,ijd->qyid', np.conjugate(V), A)
 
     DC = @ncon([Tmp1, Tmp2],[[1,-1,2,-2],[2,-3,1,-4]])
     #DC = contract('ixqa,qyid->xayd', Tmp1, Tmp2)
 
     #DC = contract('dzb,xayd->zxyab', B, DC)
-    DC = @ncon([B, DC],[[1,-1,-5],[-2,-4,-3,1]])
+    DC = @ncon([in2, DC],[[1,-1,-5],[-2,-4,-3,1]])
 
     Tmp2 = @ncon([DC, G],[[-1,-2,-3,1,2],[1,2,-4,-5]])
     #Tmp2 = contract('ijkab,abmn->ijkmn', DC, G)
 
-    A, st2, MCprime = tensorsvd(Tmp2,[1,2],[3,4,5],Dcut)
+    out1, st2, MCprime = tensorsvd(Tmp2,[1,2],[3,4,5],Dcut)
     @einsum  MCprime[i,k,l,m] = st2[i,j] * MCprime[j,k,l,m]
-    B, st3, C = tensorsvd(MCprime,[1,2],[3,4],Dcut)
+    out2, st3, out3 = tensorsvd(MCprime,[1,2],[3,4],Dcut)
 
     # Split singular piece here!
-    sing = st3^0.50 #TODO
+    sing = st3^0.500 #TODO
     # Can't take square root of matrix. May be linked to NCON error above
 
-    #B = @ncon([B, sing],[[-1,-2,1],[1,-3]])
-    #C = @ncon([sing, C],[[-1,1],[1,-2,-3]])
-    @einsum  B[i,j,p] = B[i,j,k] * sing[k,p]
-    @einsum  C[k,i,p] = sing[k,j] * C[j,i,p]
+    #out2 = @ncon([out2, sing],[[-1,-2,1],[1,-3]])
+    #out3 = @ncon([sing, out3],[[-1,1],[1,-2,-3]])
+    @einsum  out2[i,j,p] = out2[i,j,k] * sing[k,p]
+    @einsum  out3[k,i,p] = sing[k,j] * out3[j,i,p]
 
-    return A,B,C,D
+    return out1,out2,out3,out4
 
 end
 
 
-if Dcut < 4
+if Dcut < 6
         error("Increase Dcut")
     end
 
 
 temp = collect(4.5115:0.02:4.5117)
-println(temp)
 Nsteps = size(temp,1)
 f = zeros(Nsteps)
 
 for p = 1:Nsteps
 
-    A, B, C, D = Z3d_Ising(1.0/temp[p]);
-    CU = 0.0;
+    A, B, C, D = Z3d_Ising(1.0/temp[p])
+    CU = .0
 
     for iter = 1:Niter
 
@@ -253,6 +249,7 @@ for p = 1:Nsteps
         #T = contract('ika,amb,bnc,clj->ijklmn', A, B, C, D)
         T = @ncon([A, B, C, D],[[-1,-3,1],[1,-5,2],[2,-6,3],[3,-4,-2]])
         norm = maximum(T)
+        #println("Max is ", norm)
         div = sqrt(sqrt(norm))
 
         A  /= div
@@ -260,6 +257,7 @@ for p = 1:Nsteps
         C  /= div
         D  /= div
         CU += log(norm)/(2.0^(iter))
+        #println("Norm is ", CU)
 
         if iter == Niter
 
